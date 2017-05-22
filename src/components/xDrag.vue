37f6baf12d37646039b329c1fac3a8588bc955b4
gitjcc <jc_jesse@hotmail.com>
2017-05-22 18:43:58 +0800

    组件通用化

--- src/components/xDrag.vue
+++ src/components/xDrag.vue
@@ -1,39 +1,20 @@
 <template>
-  <div class="drag-root">
-    <div class="drag-title">{{model.text}}</div>
-    <draggable class="drag-group-1" v-model="model.children" :options="{group:model.group}" :move="onMove" @start="isDragging=true" @end="isDragging=false">
-      <div class="drag-item-1" v-for="aaa in model.children">
-        <div class="drag-text-1">
-          <i class="drag-item-checkbox fa" :class="aaa.checked ? 'fa-check-square-o' : 'fa-square-o'" @click="checkItem(aaa)"></i>
-          {{aaa.text}}
-          <div class="buttons">
-            <!--<span class="button" v-if="aaa.checked" @click="changeAll(aaa,false)">取消全选</span>
-                    <span class="button" v-else @click="changeAll(aaa,true)">全选</span>-->
-            <span class="button" v-if="aaa.expand" @click="expand(aaa,false)">收起</span>
-            <span class="button" v-else @click="expand(aaa,true)">展开</span>
-          </div>
-        </div>
-        <draggable class="drag-group-2" v-show="aaa.expand" :options="{group: aaa.group}" :move="onMove" @start="isDragging=true" @end="isDragging=false">
-          <div class="drag-item-2" v-for="bbb in aaa.children" :class="bbb.style">
-            <div class="drag-text-2">
-              <i class="drag-item-checkbox fa" :class="bbb.checked ? 'fa-check-square-o' : 'fa-square-o'" @click="checkItem(bbb)"></i>
-              {{bbb.text}}
-            </div>
-            <draggable class="drag-group-3" v-show="bbb.expand" :options="{group:bbb.group}" :move="onMove" @start="isDragging=true" @end="isDragging=false">
-              <div class="drag-item-3" v-for="ccc in bbb.children">
-                <span class="drag-text-3">
-                  <i class="drag-item-checkbox fa" :class="ccc.checked ? 'fa-check-square-o' : 'fa-square-o'" @click="checkItem(ccc)"></i>
-                  {{ccc.text}}
-                </span>
-              </div>
-            </draggable>
-          </div>
-        </draggable>
+  <div :id="model.id" class="drag-item" :class="['drag-item-'+model.level, model.style ? 'drag-item-block':'']">
+    <span v-show="model.text" class="drag-text" :class="'drag-text-'+model.level">
+      <i v-show="model.checkbox" @click="checkItem(model)" class="fa" :class="model.checked ? 'fa-check-square-o' : 'fa-square-o'"></i>
+      <span>{{model.text}}</span>
+      <div v-show="model.button" class="buttons">
+        <span @click="changeAll(model)">{{text.changeAll}}</span>
+        <span @click="expand(model)">{{text.expand}}</span>
       </div>
+    </span>
+    <draggable v-show="model.expand" v-model="model.children" :class="'drag-group-'+model.level"
+     :options="dragOptions" :move="onMove" @sort="onSort" @add="onAdd" @remove="onRemove" @update="onUpdate">
+      <x-drag v-for="children in model.children" :model="children" :options="options" :fn="fn">
+      </x-drag>
     </draggable>
   </div>
 </template>
-
 
 <script>
 import draggable from 'vuedraggable';
@@ -45,33 +26,116 @@ export default {
   },
   props: {
     model: Object,
+    options: Object,
     fn: Object,
   },
   data() {
     return {
-      mm: true,
+      editable: true,
+      isDragging: false,
+      delayedDragging: false,
     };
+  },
+  computed: {
+    text() {
+      return {
+        changeAll: this.model.checked ? '取消' : '全选',
+        expand: this.model.expand ? '收起' : '展开',
+      };
+    },
+    dragOptions() {
+      return {
+        group: this.model.group,
+        animation: 0,
+        disabled: false,
+        ghostClass: 'ghost',
+      };
+    },
+  },
+  watch: {
+    isDragging(newValue) {
+      if (newValue) {
+        this.delayedDragging = true;
+        return;
+      }
+      this.$nextTick(() => {
+        this.delayedDragging = false;
+      });
+    },
   },
   methods: {
     checkItem(zzz) {
       zzz.checked = !zzz.checked;
-      this.fn.onCheck(zzz);
-    },
-    changeAll(zzz, change) {
-      if (zzz.children) {
-        zzz.children.forEach(function (element) {
-          element.checked = change;
-        }, this);
-        zzz.checked = change;
+      if (this.fn && this.fn.onCheck) {
+        this.fn.onCheck(zzz.id, zzz.checked);
       }
-    },
-    expand(zzz, change) {
-      zzz.expand = change;
     },
     onMove({ relatedContext, draggedContext }) {
       const relatedElement = relatedContext.element;
       const draggedElement = draggedContext.element;
-      return (!relatedElement || !relatedElement.fixed) && !draggedElement.fixed
+      if (this.fn && this.fn.onMove) {
+        this.fn.onMove(relatedContext, draggedContext);
+      }
+      return (!relatedElement || !relatedElement.fixed) && !draggedElement.fixed;
+    },
+    onSort(event) {
+      const groupId = this.model.id;
+      const childrenIds = [];
+      this.model.children.forEach((element) => {
+        childrenIds.push(element.id);
+      });
+      const itemId = event.clone.id;
+      if (this.fn && this.fn.onSort) {
+        this.fn.onSort(groupId, childrenIds, itemId);
+      }
+      return true;
+    },
+    onAdd(event) {
+      const groupId = this.model.id;
+      const childrenIds = [];
+      this.model.children.forEach((element) => {
+        childrenIds.push(element.id);
+      });
+      const itemId = event.clone.id;
+      if (this.fn && this.fn.onAdd) {
+        this.fn.onAdd(groupId, childrenIds, itemId);
+      }
+      return true;
+    },
+    onRemove(event) {
+      const groupId = this.model.id;
+      const childrenIds = [];
+      this.model.children.forEach((element) => {
+        childrenIds.push(element.id);
+      });
+      const itemId = event.clone.id;
+      if (this.fn && this.fn.onRemove) {
+        this.fn.onRemove(groupId, childrenIds, itemId);
+      }
+      return true;
+    },
+    onUpdate(event) {
+      const groupId = this.model.id;
+      const childrenIds = [];
+      this.model.children.forEach((element) => {
+        childrenIds.push(element.id);
+      });
+      const itemId = event.clone.id;
+      if (this.fn && this.fn.onUpdate) {
+        this.fn.onUpdate(groupId, childrenIds, itemId);
+      }
+      return true;
+    },
+    changeAll(zzz) {
+      if (zzz) {
+        zzz.checked = !zzz.checked;
+        zzz.children.forEach((element) => {
+          element.checked = zzz.checked;
+        });
+      }
+    },
+    expand(zzz) {
+      zzz.expand = !zzz.expand;
     },
   },
 };
@@ -79,79 +143,90 @@ export default {
 </script>
 
 <style scoped>
-.fa {
-  font-size: 14px;
-  width: 14px;
+.drag-text .fa {
+  font-size: 16px;
+  width: 16px;
+  padding: 2px 5px;
   color: #333;
+  cursor: pointer;
 }
 
-.drag-root {
+.ghost {
+  opacity: .5;
+  background: #C8EBFB;
+}
+
+.drag-item-0 {
   display: block;
   min-height: 100px;
   margin: 10px;
-  border: 1px solid #666;
   cursor: move;
   cursor: grab;
   cursor: -moz-grab;
   cursor: -webkit-grab;
 }
 
-.drag-title {
+.drag-text-0 {
+  display: block;
   padding: 5px 10px;
+  font-size: 18px;
 }
 
-.drag-group-1 {
+.drag-group-0 {
   margin: 5px 10px;
-  min-height: 30px;
+  min-height: 100px;
+  border: 1px solid #ccc;
 }
 
 .drag-item-1 {
   display: block;
-  margin: 5px 0;
-  min-height: 30px;
-}
-
-.drag-text-1 {
-  padding: 10px 10px;
-  background: #D7D7D7;
-}
-
-.drag-group-2 {
-  min-height: 30px;
+  margin: 0 0 5px 0;
 }
 
 .drag-item-2 {
   display: inline-block;
-  min-width: 200px;
-}
-
-.drag-group-3 {
-  display: inline-block;
-  min-height: 30px;
+  padding: 5px 20px;
 }
 
 .drag-item-3 {
   display: inline-block;
-  min-height: 30px;
+  padding: 5px 5px;
+}
+
+.drag-text-1 {
+  display: block;
+  padding: 10px 10px;
+  background: #D7D7D7;
 }
 
 .drag-text-2,
 .drag-text-3 {
   display: inline-block;
-  padding: 5px 5px;
-  min-height: 30px;
+  padding: 2px 2px;
 }
 
-.drag-item-checkbox {
-  cursor: pointer;
+.drag-group-1 {
+  display: block;
+  margin: 5px 10px;
+  min-height: 20px;
+}
+
+.drag-group-2 {
+  display: inline-block;
+  min-height: 20px;
+}
+
+.drag-group-3 {
+  display: inline-block;
+  min-height: 20px;
 }
 
 .drag-item-block {
   display: block;
 }
 
-.drag-item-block .drag-text-2 {
-  min-width: 10px;
+.drag-text i {
+  cursor: pointer;
 }
 
 .buttons {
@@ -159,7 +234,7 @@ export default {
   float: right;
 }
 
-.button {
+.buttons span {
   padding: 5px 10px;
   cursor: pointer;
 }
